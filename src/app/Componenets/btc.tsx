4957a5025ab994085a68913d9f47a416fa88d351
BlueDragonDT <thomas.k99715@gmail.com>
2025-04-03 05:49:26 -0400

    frontend_final update

--- src/app/Componenets/btc.tsx
+++ src/app/Componenets/btc.tsx
@@ -2,18 +2,19 @@
 
 import React from "react";
 import StatItem from "./btc_button";
+import { MdCurrencyBitcoin } from "react-icons/md";
 
 const Btc: React.FC = () => {
   const stats = [
-    { name: "Buttcoin Price", value: "$0.006433",  },
-    { name: "Buttcoin Count", value: "₿5,384,593",  },
-    { name: "Buttcoin NAV ($M)", value: "$34,639",  },
-    { name: "Buttcoin Yield QTD", value: "11%", },
-    { name: "Buttcoin Yield YTD", value: "11%", },
-    { name: "Buttcoin Yield 2024", value: "74.3%", },
-    { name: "Buttcoin Gain QTD", value: "₿49,097", },
-    { name: "Buttcoin Gain YTD", value: "₿49,097", },
-    { name: "Buttcoin Gain 2024", value: "₿140,538", },
+    { name: "Buttcoin Price", value: "$0.006433" },
+    { name: "Buttcoin Count", value: <><MdCurrencyBitcoin className ="text-4xl text-[#fa660f] rotate-90"/> 5,384,593</> },
+    { name: "Buttcoin NAV ($M)", value: "$34,639" },
+    { name: "Buttcoin Yield QTD", value: "11%" },
+    { name: "Buttcoin Yield YTD", value: "11%" },
+    { name: "Buttcoin Yield 2024", value: "74.3%" },
+    { name: "Buttcoin Gain QTD", value: <><MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> 49,097</> },
+    { name: "Buttcoin Gain YTD", value: <><MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> 49,097</> },
+    { name: "Buttcoin Gain 2024", value: <><MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> 140,538</> }
     // { name: "BUTTCoin $ Gain QTD ($M)", value: "$4,081", change: "-0.75%" },
     // { name: "BUTTCoin $ Gain YTD ($M)", value: "$4,081", change: "+0.52%" },
     // { name: "BUTTCoin $ Gain 2024 ($M)", value: "$13,133", change: "+2.10%" },
@@ -22,7 +23,7 @@ const Btc: React.FC = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-6 bg-black">
       {stats.map((stat, index) => (
-        <StatItem key={index} name={stat.name} value={stat.value} change={stat.change} />
+        <StatItem key={index} name={stat.name} value={stat.value} />
       ))}
     </div>
   );
--- src/app/Componenets/btc_button.tsx
+++ src/app/Componenets/btc_button.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 interface StatItemProps {
   name: string;
-  value: string;
+  value: any;
   change?: string;
 }
 
@@ -16,7 +16,7 @@ const StatItem: React.FC<StatItemProps> = ({ name, value, change }) => {
   return (
     <div className="flex flex-col space-y-1">
       <span className="text-gray-400 text-lg">{name}</span>
-      <span className="text-white text-4xl font-semibold">{value}</span>
+      <span className=" flex flex-row justify-start items-center text-white text-4xl font-semibold">{value}</span>
       {change && <span className={`text-md ${changeColor}`}>{change}</span>}
     </div>
   );
