275752dab8fd30ae75212f2b332e800027bde2ff
BlueDragonDT <thomas.k99715@gmail.com>
2025-04-03 07:24:51 -0400

    frontend_finish

--- src/app/Componenets/btc.tsx
+++ src/app/Componenets/btc.tsx
@@ -5,19 +5,54 @@ import StatItem from "./btc_button";
 import { MdCurrencyBitcoin } from "react-icons/md";
 
 const Btc: React.FC = () => {
+  const currentPrice = 0.0016;
+  const currentAmount = 5384593;
+  const butt_NAV = currentPrice * currentAmount;
+  const btc_Yield_Qtd = ((currentPrice - 0.0014) / 0.0014) * 100;
+  const btc_Yield_Ytd = ((currentPrice - 0.000004) / 0.000004) * 100;
+  const btc_Yield_2024 = ((currentPrice - 0.000004) / 0.000004) * 100;
+  const btc_Gain_Qtd = currentAmount;
+  const btc_Gain_Ytd = currentAmount;
+  const btc_Gain_2024 = currentAmount;
+
   const stats = [
-    { name: "Buttcoin Price", value: "$0.006433" },
-    { name: "Buttcoin Count", value: <><MdCurrencyBitcoin className ="text-4xl text-[#fa660f] rotate-90"/> 5,384,593</> },
-    { name: "Buttcoin NAV ($M)", value: "$34,639" },
-    { name: "Buttcoin Yield QTD", value: "11%" },
-    { name: "Buttcoin Yield YTD", value: "11%" },
-    { name: "Buttcoin Yield 2024", value: "74.3%" },
-    { name: "Buttcoin Gain QTD", value: <><MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> 49,097</> },
-    { name: "Buttcoin Gain YTD", value: <><MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> 49,097</> },
-    { name: "Buttcoin Gain 2024", value: <><MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> 140,538</> }
-    // { name: "BUTTCoin $ Gain QTD ($M)", value: "$4,081", change: "-0.75%" },
-    // { name: "BUTTCoin $ Gain YTD ($M)", value: "$4,081", change: "+0.52%" },
-    // { name: "BUTTCoin $ Gain 2024 ($M)", value: "$13,133", change: "+2.10%" },
+    { name: "Buttcoin Price", value: `$${currentPrice.toFixed(4)}` },
+    { 
+      name: "Buttcoin Count", 
+      value: (
+        <>
+          <MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> {currentAmount}
+        </>
+      ) 
+    },
+    { name: "Buttcoin NAV ($M)", value: `$${butt_NAV.toFixed(2)}` },
+    { name: "Buttcoin Yield QTD", value: `${btc_Yield_Qtd.toFixed(2)}%` },
+    { name: "Buttcoin Yield YTD", value: `${btc_Yield_Ytd.toFixed(2)}%` },
+    { name: "Buttcoin Yield 2024", value: `${btc_Yield_2024.toFixed(2)}%` },
+    { 
+      name: "Buttcoin Gain QTD", 
+      value: (
+        <>
+          <MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> {btc_Gain_Qtd}
+        </>
+      ) 
+    },
+    { 
+      name: "Buttcoin Gain YTD", 
+      value: (
+        <>
+          <MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> {btc_Gain_Ytd}
+        </>
+      ) 
+    },
+    { 
+      name: "Buttcoin Gain 2024", 
+      value: (
+        <>
+          <MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> {btc_Gain_2024}
+        </>
+      ) 
+    }
   ];
 
   return (
@@ -28,4 +63,5 @@ const Btc: React.FC = () => {
     </div>
   );
 };
+
 export default Btc;
