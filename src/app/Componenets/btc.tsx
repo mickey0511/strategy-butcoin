ce71dc9be8de02a5d3f8e45c0dbe1326dc53d48b
BlueDragonDT <thomas.k99715@gmail.com>
2025-04-03 07:54:17 -0400

    frontend real finish

--- src/app/Componenets/btc.tsx
+++ src/app/Componenets/btc.tsx
@@ -7,13 +7,13 @@ import { MdCurrencyBitcoin } from "react-icons/md";
 const Btc: React.FC = () => {
   const currentPrice = 0.0016;
   const currentAmount = 5384593;
-  const butt_NAV = currentPrice * currentAmount;
+  const butt_Supply_Control = currentAmount / 1000000000 * 100;
   const btc_Yield_Qtd = ((currentPrice - 0.0014) / 0.0014) * 100;
-  const btc_Yield_Ytd = ((currentPrice - 0.000004) / 0.000004) * 100;
-  const btc_Yield_2024 = ((currentPrice - 0.000004) / 0.000004) * 100;
-  const btc_Gain_Qtd = currentAmount;
-  const btc_Gain_Ytd = currentAmount;
-  const btc_Gain_2024 = currentAmount;
+  const btc_Yield_Ytd = ((currentPrice - 0.0014) / 0.0014) * 100;
+  const btc_Yield_2024 = ((currentPrice - 0.0014) / 0.0014) * 100;
+//   const btc_Gain_Qtd = currentAmount;
+//   const btc_Gain_Ytd = currentAmount;
+//   const btc_Gain_2024 = currentAmount;
 
   const stats = [
     { name: "Buttcoin Price", value: `$${currentPrice.toFixed(4)}` },
@@ -25,34 +25,34 @@ const Btc: React.FC = () => {
         </>
       ) 
     },
-    { name: "Buttcoin NAV ($M)", value: `$${butt_NAV.toFixed(2)}` },
+    { name: "Buttcoin Supply Control(B)", value: `${butt_Supply_Control.toFixed(4)}%` },
     { name: "Buttcoin Yield QTD", value: `${btc_Yield_Qtd.toFixed(2)}%` },
     { name: "Buttcoin Yield YTD", value: `${btc_Yield_Ytd.toFixed(2)}%` },
     { name: "Buttcoin Yield 2024", value: `${btc_Yield_2024.toFixed(2)}%` },
-    { 
-      name: "Buttcoin Gain QTD", 
-      value: (
-        <>
-          <MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> {btc_Gain_Qtd}
-        </>
-      ) 
-    },
-    { 
-      name: "Buttcoin Gain YTD", 
-      value: (
-        <>
-          <MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> {btc_Gain_Ytd}
-        </>
-      ) 
-    },
-    { 
-      name: "Buttcoin Gain 2024", 
-      value: (
-        <>
-          <MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> {btc_Gain_2024}
-        </>
-      ) 
-    }
+//     { 
+//       name: "Buttcoin Gain QTD", 
+//       value: (
+//         <>
+//           <MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> {btc_Gain_Qtd}
+//         </>
+//       ) 
+//     },
+//     { 
+//       name: "Buttcoin Gain YTD", 
+//       value: (
+//         <>
+//           <MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> {btc_Gain_Ytd}
+//         </>
+//       ) 
+//     },
+//     { 
+//       name: "Buttcoin Gain 2024", 
+//       value: (
+//         <>
+//           <MdCurrencyBitcoin className="text-4xl text-[#fa660f] rotate-90" /> {btc_Gain_2024}
+//         </>
+//       ) 
+//     }
   ];
 
   return (
